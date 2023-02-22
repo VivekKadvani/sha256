f1f326814ab330867809f2563fbac10ddedb281c
Vivek Kadvani <vivek.k@simformsolutions.com>
2023-02-22 12:33:24 +0530

    optimise by removing some non needed functions

--- algo.js
+++ algo.js
@@ -1,19 +1,7 @@
 class Sha256 {
-
-  
     static hash(msg, options) {
         const defaults = { msgFormat: 'string', outFormat: 'hex' };
         const opt = Object.assign(defaults, options);
-
-        // note use throughout this routine of 'n >>> 0' to coerce Number 'n' to unsigned 32-bit integer
-
-        switch (opt.msgFormat) {
-            default: // default is to convert string to UTF-8, as SHA only deals with byte-streams
-            case 'string':   msg = utf8Encode(msg);      break;
-          
-            case 'hex-bytes':msg = hexBytesToString(msg); break; // mostly for running tests
-        }
-        
 
         // constants 64 which is get from cube root of first 64 prime number and than convert it in binary than hexa
         const K = [
@@ -24,50 +12,42 @@ class Sha256 {
             0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
             0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
             0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 ];
+            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2];
 
         // initial hash value [§5.3.3]
-        const H = [
-            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 ];
-
-        // PREPROCESSING [§6.2.1]
+        const H = [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19];
 
         msg += String.fromCharCode(0x80); //append 1 after msg after that fill zero
-     
-         // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
 
-        // convert string msg into 512-bit blocks (array of 16 32-bit integers) [§5.2.1]
-        const l = msg.length/4 + 2; // length (in 32-bit integers) of msg + ‘1’ + appended length
-        const N = Math.ceil(l/16);  // number of 16-integer (512-bit) blocks required to hold 'l' ints
+        // convert string msg into 512-bit blocks (array of 16 32-bit integers) [§5.2.1]-------------------------------------------------
+        /* calculates the length of the message in 32-bit integers,and the appended length that specifies the original length of the message in bits.*/
+        const l = msg.length / 4 + 2;
+        /* calculates the number of 16-integer blocks required to hold the l integers in the message, This ensures that there are enough 512-bit blocks to hold the entire message.*/
+        const N = Math.ceil(l / 16);
         const M = new Array(N);     // message M is N×16 array of 32-bit integers
 
-        //in this loop our input value hashed and fill i n the blocks remainiig block is filled with the 0
-        //make 16 array from 0-15
-        for (let i=0; i<N; i++) {
+        //in this loop our input value hashed and fill i n the blocks remainiig block is filled with the 0 and make 16 element arrray of 32 bit each
+        for (let i = 0; i < N; i++) {
             M[i] = new Array(16);
-            for (let j=0; j<16; j++) { // encode 4 chars per integer (64 per block), big-endian encoding
-                M[i][j] = (msg.charCodeAt(i*64+j*4+0)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16)
-                        | (msg.charCodeAt(i*64+j*4+2)<< 8) | (msg.charCodeAt(i*64+j*4+3)<< 0);
-            } // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
+            for (let j = 0; j < 16; j++) { // encode 4 chars per integer (64 per block), big-endian encoding
+                M[i][j] = (msg.charCodeAt(i * 64 + j * 4 + 0) << 24) | (msg.charCodeAt(i * 64 + j * 4 + 1) << 16)
+                    | (msg.charCodeAt(i * 64 + j * 4 + 2) << 8) | (msg.charCodeAt(i * 64 + j * 4 + 3) << 0);
+            } 
         }
-        // add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
-        // note: most significant word would be (len-1)*8 >>> 32, but since JS converts
-        // bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
-        const lenHi = ((msg.length-1)*8) / Math.pow(2, 32);
-        const lenLo = ((msg.length-1)*8) >>> 0;
-        M[N-1][14] = Math.floor(lenHi);
-        M[N-1][15] = lenLo;
-
+       
+        const lengthHi = ((msg.length - 1) * 8) / Math.pow(2, 32); // add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
+        const lengthLo = ((msg.length - 1) * 8) >>> 0;             // bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
+        M[N - 1][14] = Math.floor(lengthHi);
+        M[N - 1][15] = lengthLo;
 
         // HASH COMPUTATION [§6.2.2]
-
-        for (let i=0; i<N; i++) {
+        for (let i = 0; i < N; i++) {
             const W = new Array(64);
 
             // 1 - prepare message schedule 'W'
-            for (let t=0;  t<16; t++) {W[t] = M[i][t];}
-            for (let t=16; t<64; t++) {
-                W[t] = (Sha256.σ1(W[t-2]) + W[t-7] + Sha256.σ0(W[t-15]) + W[t-16]) >>> 0;
+            for (let t = 0; t < 16; t++) { W[t] = M[i][t]; }
+            for (let t = 16; t < 64; t++) {
+                W[t] = (Sha256.σ1(W[t - 2]) + W[t - 7] + Sha256.σ0(W[t - 15]) + W[t - 16]) >>> 0;
             }
 
             // 2 - initialise working variables a, b, c, d, e, f, g, h with previous hash value
@@ -75,9 +55,9 @@ class Sha256 {
 
             // 3 - main loop (note '>>> 0' for 'addition modulo 2^32')
             //compression function 
-            for (let t=0; t<64; t++) {
+            for (let t = 0; t < 64; t++) {
                 const T1 = h + Sha256.Σ1(e) + Sha256.Ch(e, f, g) + K[t] + W[t];
-                const T2 =     Sha256.Σ0(a) + Sha256.Maj(a, b, c);
+                const T2 = Sha256.Σ0(a) + Sha256.Maj(a, b, c);
                 h = g;
                 g = f;
                 f = e;
@@ -87,83 +67,43 @@ class Sha256 {
                 b = a;
                 a = (T1 + T2) >>> 0;
             }
-            
+
             // 4 - compute the new intermediate hash value (note '>>> 0' for 'addition modulo 2^32')
-            H[0] = (H[0]+a) >>> 0;
-            H[1] = (H[1]+b) >>> 0;
-            H[2] = (H[2]+c) >>> 0;
-            H[3] = (H[3]+d) >>> 0;
-            H[4] = (H[4]+e) >>> 0;
-            H[5] = (H[5]+f) >>> 0;
-            H[6] = (H[6]+g) >>> 0;
-            H[7] = (H[7]+h) >>> 0;
-            
+            H[0] = (H[0] + a) >>> 0;
+            H[1] = (H[1] + b) >>> 0;
+            H[2] = (H[2] + c) >>> 0;
+            H[3] = (H[3] + d) >>> 0;
+            H[4] = (H[4] + e) >>> 0;
+            H[5] = (H[5] + f) >>> 0;
+            H[6] = (H[6] + g) >>> 0;
+            H[7] = (H[7] + h) >>> 0;
         }
-
-        // for(let i=0;i<8;i++) console.log(H[i])
-        // convert H0..H7 to hex strings (with leading zeros)
-        //this function convert the decimal to hexadecimal 
-        for (let h=0; h<H.length; h++) H[h] = ('00000000'+H[h].toString(16)).slice(-8);
-        // for(let i=0;i<8;i++) console.log(H[i])
-
-        // concatenate H0..H7, with separator if 
-        //this function join them in one string and return it
-        const separator = opt.outFormat=='hex-w' ? ' ' : '';
+        //this function convert the decimal to hexadecimal  h0...h7
+        for (let h = 0; h < H.length; h++) H[h] = ('00000000' + H[h].toString(16)).slice(-8);
+        //this function join them in one string and return it H0...H7
+        const separator = opt.outFormat == 'hex-w' ? ' ' : '';
         return H.join(separator);
-
-        /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-
-        function utf8Encode(str) {
-            try {
-                return new TextEncoder().encode(str, 'utf-8').reduce((prev, curr) => prev + String.fromCharCode(curr), '');
-            } catch (e) { // no TextEncoder available?
-                return unescape(encodeURIComponent(str)); // monsur.hossa.in/2012/07/20/utf-8-in-javascript.html
-            }
-        }
-
-        function hexBytesToString(hexStr) { // convert string of hex numbers to a string of chars (eg '616263' -> 'abc').
-            const str = hexStr.replace(' ', ''); // allow space-separated groups
-            return str=='' ? '' : str.match(/.{2}/g).map(byte => String.fromCharCode(parseInt(byte, 16))).join('');
-            
-        }
     }
 
+    // Rotates right (circular right shift) value x by n positions 
+    static ROTR(n, x) { return (x >>> n) | (x << (32 - n)); }
 
-
-// Rotates right (circular right shift) value x by n positions 
-    
-    static ROTR(n, x) {
-        return (x >>> n) | (x << (32-n));
-    }
-
-
-     //Logical functions [§4.1.2].
-     
     //in this function initially calls which is rotate the value so manytimes ass given in code
-    static Σ0(x) { return Sha256.ROTR(2,  x) ^ Sha256.ROTR(13, x) ^ Sha256.ROTR(22, x); }
-    static Σ1(x) { return Sha256.ROTR(6,  x) ^ Sha256.ROTR(11, x) ^ Sha256.ROTR(25, x); }
+    static Σ0(x) { return Sha256.ROTR(2, x) ^ Sha256.ROTR(13, x) ^ Sha256.ROTR(22, x); }
+    static Σ1(x) { return Sha256.ROTR(6, x) ^ Sha256.ROTR(11, x) ^ Sha256.ROTR(25, x); }
     //in this function also rotate plus shif are performed
-    static σ0(x) { return Sha256.ROTR(7,  x) ^ Sha256.ROTR(18, x) ^ (x>>>3);  }
-    static σ1(x) { return Sha256.ROTR(17, x) ^ Sha256.ROTR(19, x) ^ (x>>>10); }
-    static Ch(x, y, z)  { return (x & y) ^ (~x & z); }          // 'choice function which is help to choice from x,y,z'
+    static σ0(x) { return Sha256.ROTR(7, x) ^ Sha256.ROTR(18, x) ^ (x >>> 3); }
+    static σ1(x) { return Sha256.ROTR(17, x) ^ Sha256.ROTR(19, x) ^ (x >>> 10); }
+    static Ch(x, y, z) { return (x & y) ^ (~x & z); }          // 'choice function which is help to choice from x,y,z'
     static Maj(x, y, z) { return (x & y) ^ (x & z) ^ (y & z); } // 'majority function which is put the value which is major from x,y,z'
-    
-
 }
-
 //this is for html logic call and return result
-document.getElementById("calculate").addEventListener("click",()=>{
-    const str=document.getElementById("string").value;
-    if(str.length==0){
+document.getElementById("calculate").addEventListener("click", () => {
+    if (document.getElementById("string").value.length == 0) {
         alert("Enter String")
-        document.getElementById("result").value=""
+        document.getElementById("result").value = ""
     }
-    else{
-    console.log(str)
-    const hash = Sha256.hash(str) 
-    document.getElementById("result").value=hash
-      console.log(hash)
-    }
-  })
+    else { document.getElementById("result").value = Sha256.hash(document.getElementById("string").value) }
+})
 
 
